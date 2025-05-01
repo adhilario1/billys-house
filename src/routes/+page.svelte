7f63f19cb04e547631c9d901b9e654a47d2b7226
adhilario1 <adam.hilario@outlook.com>
2025-05-01 00:54:19 -0700

    css trickery

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,9 @@
     import './../app.css';
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p class="test">Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="container">
+    <h1>Welcome to SvelteKit</h1>
+    <p class="test">Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+</div>
+
 
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,2 +1,9 @@
-<h1>This is the about me page</h1>
-<p>This is where the text would go... <b>IF I HAD SOME!!!</b></p>
+<script>
+    import '../../app.css'
+</script>
+
+<div class="container">
+    <h1>This is the about me page</h1>
+    <p>This is where the text would go... <b>IF <i>I</i> HAD SOME!!!</b></p>
+</div>
+
